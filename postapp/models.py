733f71f5b26b1932918c5ab2cd14516c314f9bfb
bawmee <ppr8769@likelion.org>
2019-03-07 21:00:18 +0900

    글자수줄임하아

--- postapp/models.py
+++ postapp/models.py
@@ -5,10 +5,10 @@ from django.db import models
 
 class post(models.Model):
     site = models.CharField(null = False)
-    contry = models.CharField()
-    genre = models.CharField()
-    title = models.CharField()
+    contry = models.CharField(max_length = 15)
+    genre = models.CharField(max_length = 15)
+    title = models.CharField(max_length = 15)
     rate = models.IntegerField(null = False)
-    review = models.TextField(max_length = 200, null = False)
+    review = models.TextField(max_length = 150, null = False)
     date = models.DateTimeField('date published')
-    writer = models.CharField()
+    writer = models.CharField(max_length = 15)
